912ba3613f78023ab94d8cf5d17006dd6702552d
Nishita12393 <nishitathapa1111@gmail.com>
2025-05-31 09:47:02 +1200

    initial development file

---
+++ activity.py
@@ -0,0 +1,6 @@
+import numpy as np
+integers = np.arange(10)
+print(integers)
+print(f"Shape of the array is {integers.shape}and the data type of the array is {integers.dtype}")
+multiply = integers * 2
+print (f"Each element after multiplying by 2: {multiply}")
